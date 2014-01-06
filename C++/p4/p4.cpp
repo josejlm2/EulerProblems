552c71999447f3b49772f7685026b0dcdccbafe3
josejlm2 <josejlm2@hotmail.com>
2014-01-05 21:26:30 -0600

    initial setup for p4

---
+++ C++/p4/p4.cpp
@@ -0,0 +1,27 @@
+//PROBLEM 4: LARGEST PALINDROME PRODUCT
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the 
+product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+
+#include <iostream>
+#include "math.h"
+
+bool isPalindrom(int num){
+
+
+
+}
+
+int main(){
+
+
+
+
+	std::cout << "Hello World!" << std::endl;
+	std::cin.get();
+	return 0;
+}
