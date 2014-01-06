e0f3d3d8a15989f3ea108c0cac9334b0ee3cafdc
josejlm2 <josejlm2@hotmail.com>
2014-01-06 00:00:01 -0600

    p5 complete on c++

--- C++/p5/p5.cpp
+++ C++/p5/p5.cpp
@@ -7,18 +7,58 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 
 #include <iostream>
+//Using 2520, there's not need to check for 1-10, 12, 14, 15, 18, and 20.
 
 int main()
 {
-	int number = 1;
+	long long number = 2520;
 
-	while (!(number % 20 == 0)){
-		number += 20;
+
+	while (!(number % 19 == 0))
+	{
+		std::cout << "LVL19 - " << number << std::endl;
+		if (!(number % 19 == 0) && (number % 17 == 0))
+		{
+			number += 2520;
+		}
+		while (!(number % 17 == 0))
+		{
+			std::cout << "LVL17 - " << number << std::endl;
+			if (!(number % 17 == 0) && (number % 16 == 0))
+			{
+				number += 2520;
+			}
+			while (!(number % 16 == 0))
+			{
+				std::cout << "LVL16 - " << number << std::endl;
+				if (!(number % 16 == 0) && (number % 13 == 0))
+				{
+					number += 2520;
+				}
+				while (!(number % 13 == 0))
+				{
+					std::cout << "LVL13 - " << number << std::endl;
+					if (!(number % 13 == 0) && (number%11==0))
+					{
+						number += 2520;
+					}
+					std::cout << "LVL11 - " << number << std::endl;
+					while (!(number % 11 == 0))
+					{
+						number += 2520;
+					
+					}
+
+				}
+			}
+		}
+
 	
 	}
 
+	std::cout << "ANSWER is " << number << std::endl;
 
-
+	//CORRECT ANSWER: 232792560
 	std::cin.get();
 	return 0;
 }
